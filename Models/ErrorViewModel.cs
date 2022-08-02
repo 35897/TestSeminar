97fc160c89649c70028f47c8d92ec60715b3132a
Toxic <Toxic@TOXIC>
2022-08-02 18:37:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestSeminar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
